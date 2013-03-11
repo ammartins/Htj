d4be4e341ac8237a70e768873fabe72579a5677a
root <root@DGJJ001.local>
2013-03-11 22:38:55 +0100

    Adding sparks

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = 'numero3';
 $db['default']['database'] = 'Juniores_Test';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
